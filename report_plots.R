d577fdea623f5efff9e0948f9bfd7ca19195aaa1
Lorenz Gerber <genfys@gmail.com>
2016-01-06 23:15:58 +0100

    most results and analysis finished

--- report_plots.R
+++ report_plots.R
@@ -12,10 +12,17 @@ attach(rawData)
 # aggregate data
 results<-aggregate(runTime ~ nTimes, FUN = mean)
 
-plot(results[,1], results[,2], type = 'b')
+par(mfcol=c(1,2))
+
+plot(results[,1], results[,2], type = 'b', ylab = 'runtime s', xlab='n runs')
 
 # transforming the data
-plot(results[,1], sqrt(results[,2]), type = 'b', )
+plot(results[,1], sqrt(results[,2]), type = 'b', ylab = expression(paste('runtime ', sqrt(s))), xlab = 'n runs' )
 
+summary(lm(sqrt(runTime) ~ nTimes, results))
 
+curve(5.4e-9*x^2 + 2.05e-6*x + 0.2e-4, xlim=c(0,3000), ylab="runtime s", xlab="n repetitions")
+curve(6.4e-9*x^2, add=TRUE, col='red')
 
+legend(0, 0.05, c(expression(y = 5.4%*%10^-9*x^2+2.05%*%10^-6*x+0.2%*%10^-4), 
+                  expression(y = 6.4%*%10^-9*x^2)), lty=c(1,1), lwd=c(2.5,2.5),col=c('black','red'))
